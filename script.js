06e7c16821de392669b1b8ec0d83207c0a113a78
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-18 17:42:33 -0300

    Requisições para o back-end com load(), get() e post()

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 $(document).ready(() => {
 	
+	$('#documentacao').on('click', () => {
+		//$('#pagina').load('documentacao.html')
+
+		/*
+		$.get('documentacao.html', data => { 
+			$('#pagina').html(data)
+		})
+		*/
+		$.post('documentacao.html', data => { 
+			$('#pagina').html(data)
+		})
+	})
+
+	$('#suporte').on('click', () => {
+		//$('#pagina').load('suporte.html')
+
+		/*
+		$.get('suporte.html', data => { 
+			$('#pagina').html(data)
+		})
+		*/
+		$.post('suporte.html', data => { 
+			$('#pagina').html(data)
+		})
+	})
 })
