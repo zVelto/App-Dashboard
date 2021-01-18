0d393bc358d6b2b3c3b4cb35be2b8da62e07d063
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-18 18:45:33 -0300

    Utilizando o método '$.ajax()'

--- script.js
+++ script.js
@@ -25,4 +25,25 @@ $(document).ready(() => {
 			$('#pagina').html(data)
 		})
 	})
+
+
+	//ajax
+	$('#competencia').on('change', e => {
+
+		let competencia = $(e.target).val()
+		
+		$.ajax({
+			type: 'GET',
+			url: 'app.php',
+			data: `competencia=${competencia}`, //x-www-form-urlencoded
+			dataType: 'json',
+			success: dados => { 
+				$('#numeroVendas').html(dados.numeroVendas)
+				$('#totalVendas').html(dados.totalVendas)
+			},
+			error: erro => { console.log(erro) }
+		})
+
+		//método, url, dados, sucesso, erro
+	})
 })
